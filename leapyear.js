26f1a9c4160e15179b13bef800a10c40a34e6828
Abdullah Al Ahad Bhuiyan <102456801+anondobhaii@users.noreply.github.com>
2022-08-20 15:16:58 +0600

    AnOndo

---
+++ leapyear.js
@@ -0,0 +1,34 @@
+function leapYear(year){
+    const yearDataType = typeof(year);
+    switch (yearDataType){
+        case "number":
+            if (year % 1 != 0 && year < 0){
+                console.log("Decimal and Minus number aren't allowed. Please input year with Interger Number(exp: 1972, 620,2 022 ..) ");
+            }
+            else if(year % 1 != 0){
+                console.log("Decimal number isn't allowed. Please input year with Interger Number(exp: 1972, 620,2 022 ..) ");
+            }
+            else if(year < 0) {
+                console.log("Minus number isn't allowed. Please input year with Interger Number(exp: 1972, 620, 2022 ..) ");
+            }
+            else{
+                if (year % 4 == 0 || year % 400 == 0 ){
+                    if (year % 100 != 0){
+                        console.log(year + " is a Leap Year!!")
+                    }
+                    else{
+                        console.log(year + " is not a Leap Year!!")
+                    }
+                }
+                else{
+                    console.log(year + " is not a Leap Year!!")
+                }
+            }
+            break;
+        case "string": 
+            console.log("String isn't allowed. Please input year with Interger Number(exp: 1972, 620, 2022 ..) ")
+            break;
+    }
+}
+
+leapYear();  //write year here
